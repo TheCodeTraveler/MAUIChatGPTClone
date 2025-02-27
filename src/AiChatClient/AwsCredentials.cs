662cf98e33a7d32b6518fc6d8f308f9aedddd52b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2025-02-26 16:04:30 -0800

    First commit

---
+++ src/AiChatClient/AwsCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiChatClient;
+
+    static class AwsCredentials
+{
+	public const string AccessKeyId = nameof(AccessKeyId);
+	public const string SecretAccessKey = nameof(SecretAccessKey);
+}
---
+++ src/AiChatClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Amazon;
+using Amazon.BedrockRuntime;
+using Microsoft.Extensions.AI;
+
+IAmazonBedrockRuntime runtime = new AmazonBedrockRuntimeClient(RegionEndpoint.USEast1);
+IChatClient client = runtime.AsChatClient();
+
+var chatMessage = new ChatMessage { Text = "Is this working?" };
+
+await foreach (var response in client.GetStreamingResponseAsync(chatMessage, new() { ModelId = "anthropic.claude-v2" }))
+{
+	Console.WriteLine(response.Text);
+}
