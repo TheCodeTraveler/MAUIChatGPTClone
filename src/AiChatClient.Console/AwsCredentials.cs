23085664531e1802eefdf30b8499347803115ee4
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-03-17 13:16:08 -0700

    Rename to `AiChatClient.Console`

---
+++ src/AiChatClient.Console/AwsCredentials.cs
@@ -0,0 +1,7 @@
+﻿namespace AiChatClient.Console;
+
+static class AwsCredentials
+{
+	public const string AccessKeyId = "AKIA2Q25NRJ2W5VFIEVD";
+	public const string SecretAccessKey = "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";
+}
--- src/AiChatClient.Console/Program.cs
+++ src/AiChatClient.Console/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using AiChatClient;
+﻿using AiChatClient.Console;
 using Amazon;
 using Amazon.BedrockRuntime;
 using Microsoft.Extensions.AI;
