1a298b17c2462c5bbcc9752ee16323aff9281180
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2025-05-13 11:40:48 -0700

    Update `AWSSDK.Extensions.Bedrock.MEAI`

--- src/AiChatClient.Common/BedrockService.cs
+++ src/AiChatClient.Common/BedrockService.cs
@@ -5,7 +5,7 @@ namespace AiChatClient.Common;
 
 public class BedrockService(IAmazonBedrockRuntime runtime, string modelId)
 {
-	readonly IChatClient _client = runtime.AsChatClient(modelId);
+	readonly IChatClient _client = runtime.AsIChatClient(modelId);
 
 	public IAsyncEnumerable<ChatResponseUpdate> GetStreamingResponseAsync(string input, ChatOptions options, CancellationToken token) => _client.GetStreamingResponseAsync(input, options, token);
 }
