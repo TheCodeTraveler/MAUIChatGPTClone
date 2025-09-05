b9044df1d4bd74436d72d68d743eaf5ce0ba1599
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2025-09-05 11:21:53 -0700

    Update AzureOpenAiCredentials.cs

--- src/AiChatClient.Common/Constants/AzureOpenAiCredentials.cs
+++ src/AiChatClient.Common/Constants/AzureOpenAiCredentials.cs
@@ -2,6 +2,8 @@ namespace AiChatClient.Console;
 
 public static class AzureOpenAiCredentials
 {
-	public const string Endpoint = nameof(Endpoint);
 	public const string ApiKey = nameof(ApiKey);
+	const string _endPointUrl = nameof(_endPointUrl);
+	
+    public static Uri Endpoint { get; } = new Uri(_endPointUrl);
 }
