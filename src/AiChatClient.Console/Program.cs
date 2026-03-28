c4dfd8e134a60cb10bc2c776198d92fa41d49336
Brandon Minnick <13558917+TheCodeTraveler@users.noreply.github.com>
2026-03-27 17:57:18 -0700

    Update Program.cs

--- src/AiChatClient.Console/Program.cs
+++ src/AiChatClient.Console/Program.cs
@@ -4,7 +4,7 @@ using Amazon.BedrockRuntime;
 
 using var chatClientService = new ChatClientService(
 	new AmazonBedrockRuntimeClient(AwsCredentials.AccessKeyId, AwsCredentials.SecretAccessKey, RegionEndpoint.USEast1)
-		.AsIChatClient("anthropic.claude-sonnet-4-5-20250929-v1:0"));
+		.AsIChatClient("anthropic.claude-3-haiku-20240307-v1:0"));
 
 await foreach (var response in chatClientService.GetStreamingResponseForUserAsync("Is this working?", new(), CancellationToken.None))
 {
